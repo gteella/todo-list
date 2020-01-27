0567aadbd2224e08dac8aedced4ca8a87eed5266
gteella <gteella@gmail.com>
2020-01-27 21:48:49 +0000

    Updates

---
+++ precache-manifest.2a73b9903bbfd3e4643546f2622a7727.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223feea5a92c0c5361ec0f8832b2d4c4",
+    "url": "/gteella/todo-list/index.html"
+  },
+  {
+    "revision": "a1426fccd5666fdb7476",
+    "url": "/gteella/todo-list/static/css/main.7c8f2f72.chunk.css"
+  },
+  {
+    "revision": "7b3786a6d7d166476d15",
+    "url": "/gteella/todo-list/static/js/2.5c3451f4.chunk.js"
+  },
+  {
+    "revision": "e4a6a4fbe69b52905966f9a3b585598f",
+    "url": "/gteella/todo-list/static/js/2.5c3451f4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a1426fccd5666fdb7476",
+    "url": "/gteella/todo-list/static/js/main.c53d89de.chunk.js"
+  },
+  {
+    "revision": "d7077fe1bfd0aaad3991",
+    "url": "/gteella/todo-list/static/js/runtime-main.29a939a0.js"
+  }
+]);
